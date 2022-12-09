9e58a0c1ae652f17838ba0c92450c128d1580188
azhar-sigaramtech <azhar@sigaramtech.com>
2022-12-09 12:21:03 +0530

    m-refresh

--- pages/thank-you.jsx
+++ pages/thank-you.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 // import { Paper, makeStyles, Button } from "@material-ui/core";
 // import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 
 // import { Button} from "@mui/material";
 
 import { useRouter } from "next/router";
+import { MeetContext, MNameContext } from "../context/MeetContext";
 
 const ThankYouPage = () => {
   // we will be preferring dark theme for our page
 
   // we will use this to navigate next page
   const router = useRouter();
+  const [name, setName] = useContext(MeetContext);
+  const [mName, setMName] = useContext(MNameContext);
+
+  useEffect(() => {
+    setName("");
+    setMName("");
+  }, []);
 
   return (
     <div

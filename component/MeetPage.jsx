e86ebae15bf7873616c10c1b4adad377978e9d0a
azhar-sigaramtech <azhar@sigaramtech.com>
2022-12-13 16:00:28 +0530

    Meet Name Issue

--- component/MeetPage.jsx
+++ component/MeetPage.jsx
@@ -19,7 +19,7 @@ const MeetPage = ({ query }) => {
   // INTIALISE THE MEET WITH THIS FUNCTION
   const startMeet = useCallback(() => {
     const options = {
-      roomName:{mName},
+      roomName:mName,
       width: "100%",
       height: 500,
       configOverwrite: {
